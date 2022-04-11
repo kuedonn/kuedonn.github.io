b652ca47021fcecf68a383ad5f179052e2a9fa64
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-04-11 11:46:25 +0300

    better css

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -17,8 +17,9 @@ button.addEventListener('click', function(){
     let descValue = data['weather'][0]['description'];
     let timeValue = data['dt'];
     let timeZoneValue =  data['timezone'];
+
     cityname.innerHTML = nameValue;
-    temp.innerHTML = "temperature: " + (tempValue.toFixed(1)) +"°C";
+    temp.innerHTML = "Temperature: " + (tempValue.toFixed(1)) +"°C";
     desc.innerHTML = "Current weather: " + descValue;
     time.innerHTML = "Local time: " + convertDate(timeValue,timeZoneValue);
 })
@@ -33,40 +34,96 @@ function convertDate (timeValue,timeZoneValue) {
     let seconds = "0" + date.getSeconds();
     let timeZoneStr;
 
-    switch(timeZoneValue){
+    switch(timeZoneValue){ 
+
+        //africa timezones
         case 0:
-            hours = hours - 3;
+            hours = Math.abs(hours - 3);
             timeZoneStr = "UTC";
             break;
         case 3600:
-            hours = hours - 2;
+            hours = Math.abs(hours - 2);
             timeZoneStr = "UTC+1";
             break;
         case 7200:
-            hours = hours - 1;
+            hours = Math.abs(hours - 1);
             timeZoneStr = "UTC+2";
             break;
         case 10800:
             timeZoneStr = "UTC+3"
             break;
+         
+        //north and south america timezones    
         case -32400:
-            hours = hours - 12;
+            hours = Math.abs(hours - 12);
             timeZoneStr = "UTC-9"; 
             break;
         case -28800:
-            hours = hours - 11;
+            hours = Math.abs(hours - 11);
             timeZoneStr = "UTC-8";
             break;
+        case -25200:
+            hours = Math.abs(hours - 10);
+            timeZoneStr = "UTC-7";
+            break;    
+        case -21600:
+            hours = Math.abs(hours - 9);
+            timeZoneStr = "UTC-6";
+            break;
+        case -18000:
+            hours = Math.abs(hours-8);
+            timeZoneStr = "UTC-5";
+            break;    
         case -14400:
-            hours = hours - 7;
+            hours = Math.abs(hours - 7);
             timeZoneStr = "UTC-4";
             break;
         case -10800:
-            hours = hours - 9;
-            timeZoneStr = "UTC-6";
+            hours = Math.abs(hours - 6);
+            timeZoneStr = "UTC-3";
             break;
-        case -21600:
-            hours = hours             
+        case -7200:
+            hours = Math.abs(hours - 2);
+            timeZoneStr = "UTC-2";
+            break;  
+            
+        //antarctica and arctic and asia timezones
+        case 43200:
+            hours = Math.abs(hours + 10);
+            timeZoneStr = "UTC+12"
+            break;
+        case 39600:
+            hours = Math.abs(hours + 9);
+            timeZoneStr = "UTC+11";
+            break;
+        case 36000:
+            hours = Math.abs(hours + 8);
+            timeZoneStr = "UTC+10";
+            break;
+        case 32400:
+            hours = Math.abs(hours + 7);
+            timeZoneStr = "UTC+9"    
+        case 25200:
+            hours = Math.abs(hours + 5);
+            timeZoneStr = "UTC+7";
+            break;
+        case 21600:
+            hours = Math.abs(hours + 4);
+            timeZoneStr = "UTC+6";
+            break;
+        case 19800:
+            hours = Math.abs(hours + 3) //colombo asia
+            timeZoneStr = "UTC+5:30"      
+        case 18000:
+            hours = Math.abs(hours + 3);
+            timeZoneStr = "UTC+5";
+            break;
+        case 14400:
+            hours = Math.abs(hours + 4)
+            timeZoneStr = "UTC+2";
+            break;    
+
+                     
     }
 
     let convertedTime = hours + ':' + minutes.slice(-2) + ':' + seconds.slice(-2) + ' '+ timeZoneStr;
