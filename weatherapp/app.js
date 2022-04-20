cf645dbb3ebaaa78ab763115dcbd457ae189bc77
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-04-21 00:07:09 +0300

    some typo changes and adjustments

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -21,7 +21,7 @@ button.addEventListener('click', function(){ //on submit click from user
     let tempfeelsValue = data.main.feels_like;
     let maxtempValue = data.main.temp_max;
     let mintempValue = data.main.temp_min;
-    let weatherValue = data.weather[0].main;
+    let weatherValue = data.weather[0].id;
     let descValue = data.weather[0].description;
     let timeValue = data.dt;
     let timeZoneValue =  data.timezone;
@@ -31,8 +31,8 @@ button.addEventListener('click', function(){ //on submit click from user
     cityname.innerHTML = "City: " + nameValue;
     temp.innerHTML = "Temperature: " + tempValue.toFixed(1) +"°C"; //temperature now
     feelslike.innerHTML = "Feels like: " + tempfeelsValue.toFixed(1) + "°C"; //feels like temperature
-    tempmin.innerHTML = "Min temperature: " + mintempValue.toFixed(1) + "°C"; //min day temperature (doesnt update a lot has a lot of error values)
-    tempmax.innerHTML = "Max temperature: " + maxtempValue.toFixed(1) + "°C"; //max day temperature
+    tempmin.innerHTML = "Min temperature(1h): " + mintempValue.toFixed(1) + "°C"; //min day temperature (doesnt update a lot has a lot of error values)
+    tempmax.innerHTML = "Max temperature(1h): " + maxtempValue.toFixed(1) + "°C"; //max day temperature
     desc.innerHTML = "Current weather: " + descValue; //current weather and small description
     weatherdesc(weatherValue,timeValue,timeZoneValue); //calling function to change display background image
     time.innerHTML = "Local time: " + convertDate(timeValue,timeZoneValue); //getting specific location local time from user input city name
@@ -43,44 +43,42 @@ button.addEventListener('click', function(){ //on submit click from user
 })
 
 function convertDate (timeValue,timeZoneValue) {
-    let date = new Date(timeValue*1000);  //convert epoch time to js timestamp
-    let dtime = timeZoneValue/3600;  //getting GTM time offset 
+    let date = new Date(timeValue*1000); //convert epoch time to js timestamp
+    let dtime = timeZoneValue/3600; //getting GTM time offset 
     let offset = dtime;
     let minutes = date.getMinutes();
     if (minutes>=0 && minutes<=9) minutes = "0" + minutes; //it was showing 1 digit only if minutes were lower than 10, so fixed to show correctly 0+minutes
-    if (offset > 0) offset = "+" + offset; //getting correct symbol for positive GMT time
-    return ( date.getHours()-3 + dtime + ":" + minutes + ":" + date.getSeconds() + " GMT " + offset); 
+    if (offset >= 0) offset = "+" + offset; //getting correct symbol for positive GMT time
+    return ( date.getHours()-3 + dtime + ":" + minutes + ":" + date.getSeconds() + " UTC " + offset);
     //returning the full time + offset in GMT timezone, gethours -3 for summertime, -2 for winter
 }
-
-
-//change display background dynamically with the weather id 
-function weatherdesc(weatherValue) {
+//change display background dynamically with the weather id
+function weatherdesc(weatherValue,timeValue,timeZoneValue) {
     let displayVal = document.querySelector('.display');
+    //github pages needs aboslute path like https://kuedonn.github.io/weatherapp/images/clouds.GI
     const img_urls = {
-        clouds: 'https://kuedonn.github.io/weatherapp/images/clouds.GIF',
-        clear: 'https://kuedonn.github.io/weatherapp/images/clear.JPG',
-        atmosphere: 'https://kuedonn.github.io/weatherapp/images/mist.GIF',
-        snow: 'https://kuedonn.github.io/weatherapp/images/snow.GIF',
-        rain: 'https://kuedonn.github.io/weatherapp/images/rain.GIF',
-        drizzle: 'https://kuedonn.github.io/weatherapp/images/drizzle.GIF',
-        thunderstorm: 'https://kuedonn.github.io/weatherapp/images/thunderstorm.GIF',
-        night: 'https://kuedonn.github.io/weatherapp/images/night.GIF'
+        clouds: './images/clouds.GIF',
+        clear: './images/clear.JPG',
+        atmosphere: './images/mist.GIF',
+        snow: './images/snow.GIF',
+        rain: './images/rain.GIF',
+        drizzle: './images/drizzle.GIF',
+        thunderstorm: './images/thunderstorm.GIF',
+        night: './images/night.GIF'
     };
-
     //need to get hours so i can change to night sky if its night local time
     let date = new Date(timeValue*1000);
     let dtime = timeZoneValue/3600;
-    let globalH = date.getHours()-3 + dtime;
+    let globalH = date.getHours()-3 + dtime; 
 
     switch(true) {
         case weatherValue==800:
             if (globalH<=9 || globalH>=19){
-                displayVal.style.backgroundImage = "url('"+img_urls.night+"')";
-                displayVal.style.color = "white";
-                 } else {
-                displayVal.style.backgroundImage = "url('"+img_urls.clear+"')";
-                }
+            displayVal.style.backgroundImage = "url('"+img_urls.night+"')";
+            displayVal.style.color = "white";
+            } else {
+            displayVal.style.backgroundImage = "url('"+img_urls.clear+"')";
+            }
             break;
         case weatherValue>=801 && weatherValue<=804:
             displayVal.style.backgroundImage = "url('"+img_urls.clouds+"')";
@@ -99,9 +97,9 @@ function weatherdesc(weatherValue) {
             break; 
         case weatherValue>=200 && weatherValue<=232:
             displayVal.style.backgroundImage = "url('"+img_urls.thunderstorm+"')";
-            displayVal.style.color = "white";   //thunderstorm background image is too dark so change text color to white
+            displayVal.style.color = "white";   
             break;
         default:
-            break;    
+            break;   
     }
 }
