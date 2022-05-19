3627c612c8b93c489e888444740d532c79ccf7a3
DIONSTAM <58949888+kuedonn@users.noreply.github.com>
2022-05-19 16:38:08 +0300

    added weather icons
    removed some unsed code, polished some other parts and added icons instead of weather descriptions

--- forecast/app.js
+++ forecast/app.js
@@ -14,22 +14,24 @@ button.addEventListener('click', function(){
         console.log(data)
 
         //reset table after every submit so user doesnt have to refresh page every time or brick the app
-        if (count == 0) {
-            count=1;
-        }else if (count==1){
-            for (let i=40; i>=1; i--){
-                table.deleteRow(i+1);  
-            }
+    if (count == 0) {
+        count=1;
+    }else if (count==1){
+        for (let i=40; i>=1; i--){
+            table.deleteRow(i+1);  
         }
-        //get data from 5d/3h API 
+    }
+
+    //get data from 5d/3h API 
     for (let i=0; i<40; i++){
     let tempValue = data.list[i].main.temp;
     let tempfeelsValue = data.list[i].main.feels_like;
     let maxtempValue = data.list[i].main.temp_max;
-    let descValue = data.list[i].weather[0].description;
+    let weatherValue = data.list[i].weather[0].id;
     let timeValue = data.list[i].dt_txt;
     let windValue = data.list[i].wind.speed;
     let humidityValue = data.list[i].main.humidity;
+
     //insert elements at row cells
     let row = table.insertRow(-1);
     let cell1 = row.insertCell(0);
@@ -40,16 +42,15 @@ button.addEventListener('click', function(){
     let cell6 = row.insertCell(5);
     let cell7 = row.insertCell(6);
 
-  
-    convertTempWind(tempValue, tempfeelsValue, maxtempValue, windValue, cell3, cell4, cell5, cell6);
-    cell2.innerHTML = descValue; 
+    weatherIcons(weatherValue,cell2);
+    convertTempWind(tempValue, tempfeelsValue, maxtempValue, windValue, cell3, cell4, cell5, cell6); 
     cell7.innerHTML = humidityValue + "%";
     cell1.innerHTML =  timeValue;
     }
-    
 })
 .catch(err => alert("Wrong city name"))
 })
+
 
 function convertTempWind(tempValue, tempfeelsValue, maxtempValue, windValue, cell3, cell4, cell5, cell6) { 
     let convertValue = document.getElementById('temps').value;
@@ -63,10 +64,10 @@ function convertTempWind(tempValue, tempfeelsValue, maxtempValue, windValue, cel
 
     switch(convertValue){
         case 'cms':
-              cell3.innerHTML =  tempValue.toFixed(1) +"°C"; //douleuei
-              cell4.innerHTML = tempfeelsValue.toFixed(1) + "°C";
-              cell5.innerHTML =  maxtempValue.toFixed(1) + "°C";
-              cell6.innerHTML = windValue + "m/s";
+                cell3.innerHTML =  tempValue.toFixed(1) +"°C"; 
+                cell4.innerHTML = tempfeelsValue.toFixed(1) + "°C";
+                cell5.innerHTML =  maxtempValue.toFixed(1) + "°C";
+                cell6.innerHTML = windValue + "m/s";
             break;
         
         case 'ckmh':
@@ -84,11 +85,68 @@ function convertTempWind(tempValue, tempfeelsValue, maxtempValue, windValue, cel
             break;   
             
         case 'fmph':
-              cell3.innerHTML =  tempF +"F";
-              cell4.innerHTML = feelslikeF + "F";
-              cell5.innerHTML = tempmaxF + "F";
-              cell6.innerHTML = windMPH + "mph";
+                cell3.innerHTML =  tempF +"F";
+                cell4.innerHTML = feelslikeF + "F";
+                cell5.innerHTML = tempmaxF + "F";
+                cell6.innerHTML = windMPH + "mph";
             break;
 
     }
+}
+
+//this function shows icons instead of description for the weather with id taken from API
+function weatherIcons(weatherValue,cell2) {
+
+const icons = {
+    clear: './assets/icons/clear.png',
+    clear2: './assets/icons/clear2.png',
+    clouds: './assets/icons/clouds.png',
+    clouds2: './assets/icons/clouds2.png',
+    mist: './assets/icons/mist.png',
+    rain: './assets/icons/rain.png',
+    rain2: './assets/icons/rain2.png',
+    snow: './assets/icons/snow.png',
+    sunnyclound: './assets/icons/sunnycloud.png',
+    sunnyclound2: './assets/icons/sunnycloud2.png',
+    thunderstorm: './assets/icons/thunderstorm.png'
+};
+
+    switch(true){
+        case (weatherValue>=200 && weatherValue<=232):
+            cell2.style.backgroundImage = "url('"+icons.thunderstorm+"')";
+            break;
+        
+        case (weatherValue>=500 && weatherValue<=504):
+            cell2.style.backgroundImage = "url('"+icons.rain+"')";
+            break;    
+
+        case ((weatherValue>=300 && weatherValue<=321) || (weatherValue>=520 && weatherValue<=531)):
+            cell2.style.backgroundImage = "url('"+icons.rain2+"')";
+            break;
+            
+        case ((weatherValue==511) || (weatherValue>=600 && weatherValue<=622)):
+            cell2.style.backgroundImage = "url('"+icons.snow+"')";
+            break;
+            
+        case (weatherValue<=701 && weatherValue<=781):
+            cell2.style.backgroundImage = "url('"+icons.mist+"')";
+            break;
+            
+        case (weatherValue==800):
+            cell2.style.backgroundImage = "url('"+icons.clear+"')"; 
+            break;
+            
+        case (weatherValue==801):
+            cell2.style.backgroundImage = "url('"+icons.sunnyclound+"')";
+            break;
+            
+        case (weatherValue==802):
+            cell2.style.backgroundImage = "url('"+icons.clouds+"')";
+            break;
+            
+        case (weatherValue==803 || weatherValue==804):
+            cell2.style.backgroundImage = "url('"+icons.clouds2+"')";
+            break;    
+    }
+
 }
